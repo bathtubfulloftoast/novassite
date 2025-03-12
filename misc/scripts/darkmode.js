48e564f0dedac8d941bf43de43c65249ccdda64a
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-03-11 20:34:34 -0600

    code commit

---
+++ misc/scripts/darkmode.js
@@ -0,0 +1,9 @@
+//if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//    document.getElementById("darkmode").href = "/Dark.css";
+//} else {
+//    document.getElementById("darkmode").href = "/Light.css";
+//}
+// it bugs me while im at school i dont wanna deal w it 
+// sincerest fuck yous to chrome for your dark mode not actually making things dark
+// firefox ftw babbbyyy
+
