e37b295203137e4d98108bcd32a3e01c50e8a3f7
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-03-11 21:30:37 -0600

    more shit

---
+++ secret/evil/index.php
@@ -0,0 +1,151 @@
+<head>
+<title>Novas EVIL Site</title>
+<link rel="icon" href="favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="/style.css">
+<link id="theme-style" rel="stylesheet" href="/Evil.css">
+<script src="https://kit.fontawesome.com/3c882d8d47.js" crossorigin="anonymous"></script>
+
+<!--Embed-->
+<meta content="Novas EVIL Site" property="og:title" />
+<meta content="i got some evil fucked up shit here man... visit if you DARE." property="og:description" />
+<meta content="/misc/media/embeds/evil.png" property="og:image" />
+<meta content="#ff0000" data-react-helmet="true" name="theme-color" />
+<script>
+//this is the main page im just that fucked up
+window.onload = function() {
+  changeurl();
+};
+
+function changeurl() {
+window.history.pushState("object or string", "Title", "/");
+}
+</script>
+<style>
+.banners a {
+    cursor:pointer;
+}
+
+.links i {
+    cursor:pointer;
+}
+/* ignore the spaghetti it adds to the evil */
+</style>
+</head>
+<body>
+<div class="topbox">
+<img src="EVIL.png" class="lefticon"><h1 class="headtext"> Novas EVIL Site</h1>
+</div>
+
+<div class="box">
+<b>hey there!</b><br>
+im alice and im EVIL and FUCKED UP. this is just a peep into my evil fucked up mind... hope you can handle it... i hope you HATE my EVIL BIG FUCKED UP site >:(
+</div>
+
+<div class="box">
+<b>EVIL SHIT</b><br>
+my main evil fucked up shit. THAT I MADE FULLY ALL OF THIS IS <b>MINE</b> I MADE ALL OF IT. <i>for legal reasons i didnt make any of this</i>
+<br><a href="https://modrinth.com/modpack/evil-console-idated">EVIL console-idated</a>
+<br><a href="https://youtu.be/MobkO51msMI?si=F79eaXvd9f9cCM2N">EVIL UNDERTALE THE MUSICAL</a>
+<br><a href="https://youtu.be/W1K2atmh2HY?si=DEwLSWvh2I_y77Nf">EVIL ANNOYING ORANGE.</a>
+<br><a href="https://youtu.be/LlMnDCgrhZM?si=9qIIR8nOJNmwhMHU">RUDE FUCKED UP CAT</a>
+<br><a href="https://youtu.be/7WMSQNVhMqA?si=d9mTLL_6bN1HkDVZ">MARIO IS EVIL</a>
+
+</div>
+
+<div class="box">
+<details><summary style="cursor:pointer;"><b>Evil Coding Projects</b>
+<br>very fucked up evil shit ive made
+</summary>
+<a class="sellink" href="./">EVIL</a><br>
+<a class="sellink" href="./">EVIL</a><br>
+<a class="sellink" href="./">EVIL</a><br>
+<a class="sellink" href="./">EVIL</a><br>
+<a class="sellink" href="./">EVIL</a><br>
+<a class="sellink" href="./">EVIL</a><br>
+<a class="sellink" href="./">EVIL</a><br>
+
+</details>
+</div>
+
+<div style="text-align:center" class="box">
+<div style="margin-bottom:5px;"><b>EVIL BADGES >:)</b></div><br> <!-- WHY DO I HAVE TO CREATE A DIV FOR EVERYTHING GOD DAMNIT -->
+<div class="banners">
+<a onclick="playevil1()" title="EVIL"><img src="evil1.gif"></a>
+<audio id="evil1" src="evil1.ogg"></audio>
+
+<a onclick="playevil2()" title="EVIL"><img src="evil2.gif"></a>
+<audio id="evil2" src="evil2.ogg"></audio>
+
+<a onclick="playevil3()" title="EVIL"><img src="evil3.gif"></a>
+<audio id="evil3" src="evil3.ogg"></audio>
+
+<a onclick="playevil4()" title="EVIL"><img src="evil4.gif"></a>
+<audio id="evil4" src="evil4.ogg"></audio>
+
+<a onclick="playevil5()" title="EVIL"><img src="evil5.gif"></a>
+<audio id="evil5" src="evil5.ogg"></audio>
+
+<script>
+function playevil1() {
+    var x = document.getElementById("evil1");
+    x.play();
+}
+
+function playevil2() {
+    var x = document.getElementById("evil2");
+    x.play();
+}
+
+function playevil3() {
+    var x = document.getElementById("evil3");
+    x.play();
+}
+
+function playevil4() {
+    var x = document.getElementById("evil4");
+    x.play();
+}
+
+function playevil5() {
+    var x = document.getElementById("evil5");
+    x.play();
+}
+</script>
+
+</div>
+</div>
+
+<div class="box">
+<div class="links">
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+<i title="EVILLLLL" class="fa-solid fa-skull"></i>
+
+
+</div>
+</div>
+
+
+
+<div class="footer">
+<div>Novas EVIL Site <?php echo date("Y")-69; ?></div>
+
+<div>evil@novassite.net</div>
+</body>
