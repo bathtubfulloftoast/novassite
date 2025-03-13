f7b2f0edc591a37d723eefbb8ca0b2b9f42d45a4
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-03-13 00:08:43 -0600

    move to astro.

---
+++ src/assets/age.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    const birthTimestamp = 1181368800 * 1000; // Convert the Unix timestamp to milliseconds
+    const currentTimestamp = Date.now();
+
+    // Calculate the difference in milliseconds
+    const timeDifference = currentTimestamp - birthTimestamp;
+
+    // Convert milliseconds to years
+    const years = timeDifference / (1000 * 60 * 60 * 24 * 365.25); // Approximate years (365.25 to account for leap years)
+
+// Display the age
+const currentAge = `${Math.floor(years)} year`;
+const ageElement = document.getElementById("age");
+
+if (ageElement) {
+    ageElement.innerHTML = currentAge; // Update the element if it exists
+} else {
+    console.error('Element with ID "age" not found.');
+}
+});
