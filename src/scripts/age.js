c870d5f5d0b34fa1a1b5546d3a9e53c64ace7e4f
Nova <bathtubfulloftoast@gmail.com>
2025-04-04 07:38:23 -0600

    remove garbage

--- src/scripts/age.js
+++ src/scripts/age.js
@@ -6,15 +6,9 @@ document.addEventListener('DOMContentLoaded', async function() {
     const timeDifference = currentTimestamp - birthTimestamp;
 
     // Convert milliseconds to years
-    const years = timeDifference / (1000 * 60 * 60 * 24 * 365.25); // Approximate years (365.25 to account for leap years)
+    const years = timeDifference / (1000 * 60 * 60 * 24 * 365.25);
 
 // Display the age
 const currentAge = `${Math.floor(years)} year`;
-const ageElement = document.getElementById("age");
-
-if (ageElement) {
-    ageElement.innerHTML = currentAge; // Update the element if it exists
-} else {
-    console.error('Element with ID "age" not found.');
-}
+document.getElementById("age").innerHTML = currentAge; 
 });
