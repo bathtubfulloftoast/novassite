2e2eddf621db3d983944d44b2d513b8cd1bee110
Nova <bathtubfulloftoast@gmail.com>
2025-04-23 07:59:02 -0600

    byte conversion

--- src/pages/deviceinfo.txt.js
+++ src/pages/deviceinfo.txt.js
@@ -3,6 +3,19 @@ import fs from 'fs';
 
 let distro;
 
+export function formatBytes(bytes, decimals = 2) {
+    if (!+bytes) return '0 Bytes'
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+// https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+
 export function getLinuxDistro() {
     try {
         const releaseInfo = fs.readFileSync('/etc/os-release', 'utf-8');
@@ -39,7 +52,6 @@ const hostname = os.hostname(); //supernova
 const cpumodel = os.cpus()?.[0].model; // AMD Ryzen 7 5700U with Radeon Graphics
 const device = getDevice();
 const memory = os.totalmem(); // returned in bytes 
-// do the byte calculation shit when you get home idiot.
 
 if(platform == "linux") {
     distro = getPrettyDistro();
@@ -55,7 +67,7 @@ os: ${distro}
 architecture: ${architecture}
 platform: ${cpuplatform}
 cpu: ${cpumodel}
-ram: ${memory}
+ram: ${formatBytes(memory)}
 device: ${device}`
 );
     } catch (error) {
