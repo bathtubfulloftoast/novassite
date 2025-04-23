6abec594f2afd46112837c3e55b10a93a2a6797d
Nova <bathtubfulloftoast@gmail.com>
2025-04-23 07:51:07 -0600

    MORE INFO

--- src/pages/deviceinfo.txt.js
+++ src/pages/deviceinfo.txt.js
@@ -13,6 +13,16 @@ export function getLinuxDistro() {
     }
 }
 
+export function getPrettyDistro() {
+    try {
+        const releaseInfo = fs.readFileSync('/etc/os-release', 'utf-8');
+        const match = releaseInfo.match(/^PRETTY_NAME="([^"]*)"/m);
+        return match ? match[1] : 'Generic Linux';
+    } catch (err) {
+        return 'Generic Linux';
+    }
+}
+
 export function getDevice() {
     try {
         const releaseInfo = fs.readFileSync('/sys/devices/virtual/dmi/id/product_name', 'utf-8');
@@ -24,12 +34,15 @@ export function getDevice() {
 
 const platform = os.platform(); // linux
 const architecture = os.arch(); // x64
+const platform = os.machine(); // x86_64
 const hostname = os.hostname(); //supernova
 const cpumodel = os.cpus()?.[0].model; // AMD Ryzen 7 5700U with Radeon Graphics
 const device = getDevice();
+const memory = os.totalmem(); // returned in bytes 
+// do the byte calculation shit when you get home idiot.
 
 if(platform == "linux") {
-    distro = getLinuxDistro();
+    distro = getPrettyDistro();
 } else {
     distro = platform;
 }
@@ -39,8 +52,10 @@ export async function GET() {
 return new Response(
 `hostname: ${hostname}
 os: ${distro}
-arch: ${architecture}
+architecture: ${architecture}
+platform: ${platform}
 cpu: ${cpumodel}
+ram: ${memory}
 device: ${device}`
 );
     } catch (error) {
