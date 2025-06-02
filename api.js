6a12494d8d6efa109c005f0be9195ef40b60bafd
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-06-01 18:35:38 -0600

    properly added guestbook.
    yay

--- api.js
+++ api.js
@@ -4,8 +4,8 @@ import qrAPI from './functions/qr.js';
 import discorduserAPI from './functions/discord.js';
 import discordinviteAPI from './functions/invite.js';
 import uptime from './functions/uptime.js';
-// import guestpost from './functions/writeguestbook.js';
-// import getguest from './functions/readguestbook.js';
+import guestpost from './functions/writeguestbook.js';
+import getguest from './functions/readguestbook.js';
 
 export default function registerAPIRoutes(app) {
 app.get('/api/lastfm', lastfmAPI);
@@ -14,7 +14,7 @@ app.get('/api/qr', qrAPI);
 app.get('/api/discord', discorduserAPI);
 app.get('/api/invite', discordinviteAPI);
 app.get('/api/uptime', uptime);
-// app.post('/api/guestbook', guestpost);
-// app.get('/api/guestbook', getguest);
+app.post('/api/guestbook', guestpost);
+app.get('/api/guestbook', getguest);
 
 }
