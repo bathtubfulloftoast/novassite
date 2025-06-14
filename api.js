2d2ada5d857852401ebd6e8479401c169109fa13
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-06-14 02:02:07 -0600

    special bot stuff

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 // import lastfmAPI from './functions/lastfm.js';
 // import openweatherAPI from './functions/openweather.js';
 // import qrAPI from './functions/qr.js';
-// import discorduserAPI from './functions/discord.js';
 // import discordinviteAPI from './functions/invite.js';
 // import uptime from './functions/uptime.js';
 import dspresence from './functions/presence.js';
@@ -10,8 +9,7 @@ export default function registerAPIRoutes(app) {
 // app.get('/api/lastfm', lastfmAPI);
 // app.get('/api/openweather', openweatherAPI);
 // app.get('/api/qr', qrAPI);
-// app.get('/api/discord', discorduserAPI);
 // app.get('/api/invite', discordinviteAPI);
 // app.get('/api/uptime', uptime);
-app.get('/api/presence', dspresence);
+app.get('/api/discord', dspresence);
 }
--- functions/presence.js
+++ functions/presence.js
@@ -10,7 +10,24 @@ export default async function presenceHandler(req, res) {
             return res.status(404).json({ error: 'Presence not found for user' });
         }
 
-        res.status(200).json(member.presence);
+        res.status(200).json(
+            {
+                id: member.user.id,
+                bot: member.user.bot,
+                username: member.user.username,
+                nickname: member.user.globalName,
+                status: member.presence.status,
+                clientstatus: member.presence.clientStatus,
+
+                avatar: member.user.avatar,
+                creationtime: member.user.createdTimestamp,
+                discriminator: member.user.discriminator, //for bots.
+                tag: member.user.tag, //for bots.
+
+                activities: member.presence.activities,
+
+            }
+        );
     } catch (error) {
         console.error('Error fetching presence:', error);
         res.status(500).json({ error: 'Internal server error' });
