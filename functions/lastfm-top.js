88bea00cd90886435b953025630967dc377a24de
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-06-14 20:32:46 -0600

    hopefully finished for the main profile page

---
+++ functions/lastfm-top.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+
+let cache = {};
+
+export default async function lastfmHandler(req, res) {
+    const API_KEY = process.env.LASTFM_API_KEY;
+    const USER = "bathtuboftoast";
+    const MAXFM = "5";
+    const CACHE_DURATION = 600000000; // 1 week
+
+    if (cache.timestamp && (Date.now() - cache.timestamp < CACHE_DURATION)) {
+        const remaining = CACHE_DURATION - (Date.now() - cache.timestamp);
+        return res.status(200).json({
+            ...cache.data,
+            cache_remaining: Math.floor(remaining/1000),
+        });
+    }
+
+    const url = `http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user=${USER}&api_key=${API_KEY}&format=json&limit=${MAXFM}&period=1month`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        cache = {
+            data,
+            timestamp: Date.now(),
+        };
+
+        res.status(200).json({
+            ...data,
+            cache_remaining: Math.floor(CACHE_DURATION/1000),
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+}
