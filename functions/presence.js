64453ae07edc44576b738440cfb737a6a7107b2b
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-06-17 17:32:00 -0600

    add caching/rate limiting

--- functions/presence.js
+++ functions/presence.js
@@ -1,8 +1,20 @@
 import { client } from '../discordBot.js';
 
+let cache = {};
+
 export default async function presenceHandler(req, res) {
+    const CACHE_DURATION = 120000;
     const userId = '471806482648924174';
     const guild = client.guilds.cache.get('1264262104043618336');
+
+    // Check for valid cache
+    if (cache.timestamp && (Date.now() - cache.timestamp < CACHE_DURATION)) {
+        const remaining = CACHE_DURATION - (Date.now() - cache.timestamp);
+        return res.status(200).json({
+            ...cache.data,
+            cache_remaining: Math.floor(remaining / 1000),
+        });
+    }
 
     try {
         const member = await guild.members.fetch(userId);
@@ -10,24 +22,29 @@ export default async function presenceHandler(req, res) {
             return res.status(404).json({ error: 'Presence not found for user' });
         }
 
-        res.status(200).json(
-            {
-                id: member.user.id,
-                bot: member.user.bot,
-                username: member.user.username,
-                nickname: member.user.globalName,
-                status: member.presence.status,
-                clientstatus: member.presence.clientStatus,
+        const data = {
+            id: member.user.id,
+            bot: member.user.bot,
+            username: member.user.username,
+            nickname: member.user.globalName,
+            status: member.presence.status,
+            clientstatus: member.presence.clientStatus,
+            avatar: member.user.avatar,
+            creationtime: member.user.createdTimestamp,
+            discriminator: member.user.discriminator,
+            tag: member.user.tag,
+            activities: member.presence.activities,
+        };
 
-                avatar: member.user.avatar,
-                creationtime: member.user.createdTimestamp,
-                discriminator: member.user.discriminator, //for bots.
-                tag: member.user.tag, //for bots.
+        cache = {
+            data,
+            timestamp: Date.now(),
+        };
 
-                activities: member.presence.activities,
-
-            }
-        );
+        res.status(200).json({
+            ...data,
+            cache_remaining: Math.floor(CACHE_DURATION / 1000),
+        });
     } catch (error) {
         console.error('Error fetching presence:', error);
         res.status(500).json({ error: 'Internal server error' });
