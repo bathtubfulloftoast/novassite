3e8632e2c00ec502fddf9bd0aca90095e08e69d1
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-06-29 17:40:07 -0600

    add recent games (from steam)

---
+++ src/scripts/steamrecent.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", async function() {
+
+async function weather() {
+    const response = await fetch('/api/steam-recent');
+    let data = await response.json();
+
+
+let text = "";
+data.response.games.forEach(myFunction);
+
+document.getElementById("recentgames").innerHTML = text;
+
+function myFunction(item, index) {
+const gamename = item.name || "No Name";
+const appid = item.appid || "10";
+const playtime = item.playtime_2weeks || 0; // i have this data im gonna use it okay
+
+let playtimeformat = "";
+if(playtime > 60) {
+playtimeformat = Math.round(playtime/60)+"H";
+} else {
+playtimeformat = `${playtime}M`;
+}
+
+text += `${playtimeformat} | <a href="https://store.steampowered.com/app/${appid}" target="_blank">${gamename}</a><br>`;
+}
+
+
+}
+await weather();
+});
