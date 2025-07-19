362266ae8f87ad45a812330808fc2a5cbae5f790
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-07-19 12:46:23 -0600

    server join/leave things

---
+++ botevents/memberleave.js
@@ -0,0 +1,11 @@
+const JCHANNEL = process.env.JOINCHANNEL;
+const GUILDID = process.env.PRESENCE_GUILDID;
+
+export function MemberLeave(client) {
+client.on("guildMemberRemove", member => {
+if(member.guild.id == GUILDID) {
+console.log(`${member.user.username} left the server.`);
+client.channels.cache.get(JCHANNEL).send(`<@${member.user.id}> (aka **${member.user.globalName}**) left the server.`);
+}
+});
+}
