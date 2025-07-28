f52d2a189260c21c7187d4722acbee2d82c739d2
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-07-27 22:47:11 -0600

    gave bot about me page :3

---
+++ functions/botinfo.js
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+import colors from 'colors';
+
+import { client } from '../discordBot.js';
+
+let cache = {};
+
+export default async function presenceHandler(req, res) {
+    const CACHE_DURATION = 60000;
+
+    // Check for valid cache
+    if (cache.timestamp && (Date.now() - cache.timestamp < CACHE_DURATION)) {
+        const remaining = CACHE_DURATION - (Date.now() - cache.timestamp);
+        return res.status(200).json({
+            ...cache.data,
+            cache_remaining: Math.floor(remaining / 1000),
+        });
+    }
+
+    try {
+
+
+        const data = {
+            user: client.user,
+            commands: client.commands,
+            guildcount:client.guilds.cache.map(guild => guild.id).length,
+            activities: client.presence.activities,
+        };
+
+        cache = {
+            data,
+            timestamp: Date.now(),
+        };
+
+        res.status(200).json({
+            ...data,
+            cache_remaining: Math.floor(CACHE_DURATION / 1000),
+        });
+    } catch (error) {
+        console.error('Error fetching presence:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    console.log(`${colors.green("[Site]")} grabbed bot info`);
+}
