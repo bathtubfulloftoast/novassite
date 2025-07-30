b8ed30e514a963b4f007de24b20acc1d2fe2d23a
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-07-29 20:20:37 -0600

    more commands !!!

---
+++ botcommands/image/short.js
@@ -0,0 +1,73 @@
+import { SlashCommandBuilder,AttachmentBuilder,MessageFlags} from 'discord.js';
+import colors from 'colors';
+import sharp from 'sharp';
+import color from 'color';
+
+let image = "";
+let unfortunate = "";
+
+export default {
+    data: new SlashCommandBuilder()
+    .setName('short')
+    .setDescription('declare someone as SHORT')
+    .addUserOption(option =>
+    option.setName('user')
+    .setDescription('Attachment User')
+    .setRequired(false)),
+
+async execute(interaction) {
+await console.log(`${colors.cyan("[Discord]")} command short has been run by ${interaction.user.tag}`);
+const attacheduser =  interaction.options.getUser('user');
+
+try {
+if (attacheduser) {
+image = await fetch(`https://cdn.discordapp.com/avatars/${attacheduser.id}/${attacheduser.avatar}.webp?size=1024`);
+unfortunate = attacheduser.id;
+}
+
+else {
+image = await fetch(`https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.webp?size=1024`);
+unfortunate = interaction.user.id;
+}
+
+    const buffer = await image.arrayBuffer();
+    const imagebuffer = Buffer.from(buffer);
+
+    // Load base and overlay images
+    const overlayImage = sharp(imagebuffer).resize({width:600, height:193,fit:"fill"});
+    const baseImage = sharp('public/bot/SHORT.png');
+
+    // Get metadata of overlay image
+    const overlayMetadata = await overlayImage.metadata();
+    const overlayBuffer = await overlayImage.toBuffer();
+
+    const left = 0; // Bottom-left means x = 0
+    const top = 607; // y = total height - overlay height
+
+    const final = await baseImage
+    .composite([
+        {
+            input: overlayBuffer,
+            top: top,
+            left: left,
+        },
+    ])
+    .toFormat("jpg")
+    .toBuffer();
+
+    const file = await new AttachmentBuilder(final);
+    file.setName("SHORT.jpg");
+    file.setDescription("youre fucking SHORT.");
+
+    await interaction.reply({content: `<@${unfortunate}> is fucking ***SHORT***`,files:[file]});
+
+} catch (err) {
+await interaction.reply({content:"command failed to run", flags: MessageFlags.Ephemeral});
+console.error("Error:", err);
+}
+
+
+
+},
+};
+
