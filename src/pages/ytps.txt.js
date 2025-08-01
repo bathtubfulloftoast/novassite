407e0150117e12eba98a81658943814d2e396554
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-07-31 21:31:30 -0600

    kill sauce array 😔

---
+++ src/pages/ytps.txt.js
@@ -0,0 +1,135 @@
+// hello and welcome to defining this so i hopefully dont forget what this is about like i did last night
+// okay to get on this list you must be one of these things
+// 1. a litterally YTP/YTPMV
+// 2. funnier old video that was for sure a shitpost or just "fits the theme" (ex: Rainbow Road, machine gun grandma)
+// 3. a mashup shitpost (ex: [BIG SHOT]'s World, OPPA SPAMTON STYLE)
+// 4. rarely allowed: an old video that should inflict pain due to "ow im old" (ex: Nyan Cat - Genre Hopping)
+// none of this actually matters its just kind of how my internal thought process is.
+
+// also this is done this way instead of just a regular txt file because i would like to know what to look for in case a video is deleted.
+// also cause the users gonna find out what video theyre watching pretty fast tbh
+
+const sauce = [
+"1qVRxy9fuTU", // Spy Stole The Precious Intelligence
+"3YGst2a--t8", // [YTPMV] Chaos Robotking (Deltarune)
+"3hPLhIUtoD4", // i fownd a inaproptiate plase!!!!!!.3gp
+"6-DLSjIpMmA", // OPPA SPAMTON STYLE
+"6Ajhzlq42f0", // Super Spice Bros 2
+"7KMTeJdazK4", // (YTP) Spencer Launches a Bagel into Griffin's Rectum
+"9BaWt_BrYUg", // top ten spongebob funny faces.wmv
+"BCPkXp9K0D0", // [FULL VERSION] Lagtrain but it's Soldier from TF2
+"CYXJYQZ3FX0", // machine gun grandma
+"FuX5_OWObA0", // Rainbow Road
+"GTh5J0HsIAg", // Morshu Reads the Entire Bible! (no AI)
+"Hijh2ox9CEg", // In The Lagtrain (Full Version) [HL2 Music Video]
+"HmzGmmq0Jms", // A JERMA'S WORLD?
+"NagoHr74Jko", // In The California End Mashup (Full Version)
+"PGJKeESLBpQ", // Grandpa Found the Car Keys
+"PGfhtLrwiKo", // 🎩
+"U4eVHaeYahU", // [BIG SHOT]'s World
+"UUbS6UjkfBM", // WALKIN'
+"U_CaPYe5kEs", // KRABBY!
+"VZ3DEnvJ464", // 3 BIG SHOTS FROM [KITCHEN GUN]
+"VoWRSM2_DF0", // 👖🍎
+"XBMg5s_TZWk", // MrBeast Stole The Precious Thing [YTPMV]
+"YDQ3mvd64Uc", // KITCHENLOVANIA
+"YidRNvL535I", // Super Mario Bros Condom Trumpet
+"ZsUoefdaoo0", // Patty Power
+"bfBO5dGlmrE", // Jerma's Mansion
+"c9ilXkkwGu4", // Game Over - Vs. Cyborg OST
+"ddWJatRxfz8", // The Glorious Octagon of Destiny
+"eGrg6JHlByw", // Jerma Owes the Precious 2.5 Billion Dollars
+"ePw3wZxdGio", // clinton kong country 2: bill's kong quest
+"fU3jfk-O7L0", // SPICETALE
+"klwIOSCJFSA", // [YTP] Jim David Gilbert's Teleporting Soup
+"lFl1_3Eq__0", // You got the dud V3.0
+"lSc-t9YBvkk", // Another Mediocre Undertale YTPMV
+"rG_Ue3t17l0", // [YTP] Good Old P.U.
+"rjRV0G6qWgw", // ReDoin
+"ruM7pRYjCH0", // [MASHUP] In The Virtual Bad Apple (FULL VERSION) [HL2 Music Video]
+"rzLIUgnKY40", // future gen. consels wii2 ps4 xbox720?
+"tcjCfJbYAzg", // Kitchen Gun - 3 Shots Of Derek Bum
+"upf_xYgkBIQ", // meet team
+"iV0aIfJPT-w", // 네, 선장님!
+"mAQDkPrM1uI", // Mario Canyon
+"2XvILfdj52Q", // Youtube Poop - Donald Mcronald
+"_wYQAGANgL0", // [[YTPMV]] BIG SHOT
+"AWDeOiZHPhY", // [[YTPMV]] this will be DELTARUNE in 2013
+"vcPvbojUmuw", // Artificial Intelligence Beach
+"kBKr8YLuVgs", // Osmium Alloy Bomb
+"lRpxecLkUHQ", // Nyan Cat - Genre Hopping
+"hiRacdl02w4", // you are an idiot!
+"Q16KpquGsIc", // Ronald McDonald insanity
+"f8tGW5B43o8", // ♪ UNDERTALE THE MUSICAL ft SKRILLEX - Animation Song Parody Remix
+"w6bIKJHiaLo", // Another Medium fans when they meet an Another Easy fan
+"CwWvNA5eJxw", // Bagels I Didn't Even Want
+"Ja5BDtMybBA", // {YTP} ~ Speedfolded
+"7XvUCC9-y50", // [YTP] st
+"BeLTb17dioM", // [YTP] The History of Summoning Salt YouTube Poops
+"5haJCngMP1I", // Mark Cerny double crosses playstation
+"sJXag2HYT94", // [YTP] That's Moe Like It
+"0SPjy9o20QQ", // {YTP} ~ (Prolly) Troblems
+"x3NhDbzD_zw", // [YTP] everybody dies
+"JkaMIFqm5cI", // [YTP] INCREDIBLY HOT DULLICIOUS PISSA
+"dQNN4Lvmja8", // Minecraft Movie YTPMV はいよろこんで
+"n6TdJ8-reM0", // What Della Knows [YTPMV]
+"3Nv2GDsz-58", // [YTP] Mild frustration over grammar
+"AM2SRY0ea9c", // [YTP] We are squirting with PRECISION and ELEGANCE out here
+"yJQrx5Ulq7M", // [YTP] Wipe
+"4xYvF2CCHG0", // {YTP} ~ A Guy's DDDDD&D
+"rNe-gK22LV4", // lemons
+"8t9LtVCHo1A", // ルイージのグルメレース
+"Yxm2W0hDTIs", // 成田スカイアクセスは大変なものを運んでいきました
+"GxW31NhNiUo", // マリオのグルメレース
+"0VZ4rQtLZQs", // 愛犬ロボレジわろす
+"aqzbqKypOMk", // It Starts With Whistle
+"uyYWJhWy7RU", // Everything's CYBER In The Future
+"O36f1p8QSaw", // YTPMV: Big Beat Mario
+"iYGTbrYi914", // doin
+"f-Zb1MojVoM", // [YTP] Crustal Poopsy Sales Sales Profits Sales
+"Ivim1WSCFgY", // family man pilot YTP
+"R0btD4cpEVU", // [YTP] Man Loses His Sausage
+"0XoaeeSeGvA", // [YTP] For Goodness Sake
+"IQfbFRNgXPY", // [YTP] Jerry Rigger and the Forbidden Phone
+"kzCpyY9PW4k", // Nightcore - Are You Gonna Eat That? - Rhett & Link
+"3MnrAw8Icfs", // YouTube Poop: The Sky Had A Weegee!
+"3XE6zA9D8uY", // Youtube poop-haunted house of weegee!
+"y5qEPTH3ntw", // EVERYBODY DO THE はいよろこんで
+"6DJ3LfZhLKM", // wow
+"QEE_LJNROww", // scary_bird.avi
+"_XRoEWwxOrA", // [YTP] PragerU is wild
+"9UG9xwfInPY", // Toy homer- d'oh!!
+"I6X_rUxx-iw", // Cat-ish
+"BZvhCPEd1jk", // (YTP) 'New Super Luigi's Long Legs U Deluxe' Launch Trailer
+"lsrsUy3C07A", // YTP: Hotel Mario Maker
+"PQNgQGA0I90", // YouTube Poop-Weegee Takes Over Spongebob
+"N43sS-pDaHE", // pronouns guy just wants his waffles
+"b4dhm_CVm_0", // {YTP} ~ ultrasdf
+"0XKNEEqDHPk", // [YTP] potato
+]
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    while (currentIndex !== 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
+    }
+    return array; // Add this line to return the shuffled array
+}
+
+export async function GET() {
+    try {
+
+        const shuffled = shuffle(sauce);
+        const textContent = shuffled.join("\n");
+
+        return new Response(textContent);
+    } catch (error) {
+        return new Response("Error: File not found");
+    }
+}
