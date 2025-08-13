5c228e0179a29d6a70009552c42123c6abe81da4
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-08-12 18:00:00 -0600

    enable https support (also generated swag ass fake certificate)

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import expressip from 'express-ip';
+import http from 'http';
+import https from 'https';
 import bodyParser from 'body-parser'
 import colors from 'colors';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
@@ -10,12 +12,20 @@ import fs from 'fs';
 import {marked} from 'marked';
 
 let port = process.env.PORT;
+let protocol = process.env.PROTOCOL.toLowerCase();
+
+var key = fs.readFileSync('fakecert.key');
+var cert = fs.readFileSync('fakecert.crt');
+var options = {
+    key: key,
+    cert: cert
+};
 
 const app = express();
 const base = '/';
 
 function startServer() {
-if(!port) {
+if(!port || isNaN(port)) {
 port = 8080;
 console.log(`${colors.red("[ERROR]")} config invalid please check site.`);
 
@@ -25,7 +35,7 @@ console.error(err);
 return;
 }
 app.get('/', (req, res) => {
-res.send(`<head><title>ERROR</title><style>table{font-family:arial,sans-serif;border-collapse:collapse;}td,th{border:1px solid #dddddd;text-align:left;padding:8px;}tr:nth-child(even){background-color:#dddddd;}code{background-color:#dddddd;}</style></head> no port set in .env file<br>you can look at the README below<br><hr>${marked.parse(data)}<hr><br><b>server has been stopped</b>`);
+res.send(`<head><title>ERROR</title><style>table{font-family:arial,sans-serif;border-collapse:collapse;}td,th{border:1px solid #dddddd;text-align:left;padding:8px;}tr:nth-child(even){background-color:#dddddd;}code{background-color:#dddddd;}</style></head> no port or invalid port set in .env file<br>you can look at the README below<br><hr>${marked.parse(data)}<hr><br><b>server has been stopped</b>`);
 console.log(`${colors.green("[Site]")} error page loaded, stopping server.`);
 process.exit(0);
 })
@@ -50,8 +60,16 @@ app.use((req, res) => {
     res.status(404).sendFile('dist/client/404.html', { root: '.' });
 });
 
-app.listen(port, () => {
-    console.log(`${colors.green("[Site]")} Server running at http://localhost:${port}`);
+if (protocol == "https") {
+var server = https.createServer(options, app);
+protocol = "https";
+} else {
+var server = http.createServer(app);
+protocol = "http";
+}
+
+server.listen(port, () => {
+    console.log(`${colors.green("[Site]")} Server running at ${protocol}://localhost:${port}`);
 });
 }
 
