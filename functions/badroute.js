42ebc8c8e80f750f07206fd0859d58bc66418aaa
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-08-16 01:01:13 -0600

    add error handling for invalid api calls.

---
+++ functions/badroute.js
@@ -0,0 +1,13 @@
+import colors from 'colors';
+
+export default async function lastfmHandler(req, res) {
+const route = req.params.route;
+
+res.set('Content-Type', "text/plain");
+res.status(404).send("invalid api route\nmaybe it was removed?");
+
+if(route) { // only log if it *might* be my fault
+console.log(`${colors.red("[Site]")} function ${route} was called bot not found.`);
+}
+
+}
