30f51dec8b9c8de4f6dffd644cf4f5973bbd3b62
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-08-16 15:09:46 -0600

    webcam doesn't immediately fire if requested on web.

--- functions/webcam.js
+++ functions/webcam.js
@@ -1,7 +1,11 @@
 import NodeWebcam from 'node-webcam';
 import colors from 'colors';
+import fs from 'fs';
+
+let rcache;
 
 export default async function lastfmHandler(req, res) {
+const CACHE_DURATION = 60000;
 
 var opts = {
     width: 480,
@@ -17,8 +21,19 @@ var opts = {
 
 res.set('Content-Type', "image/jpeg");
 
-await NodeWebcam.capture( "webcam", opts, function( err, data ) {
+if (rcache && (Date.now() - rcache < CACHE_DURATION)) {
+const remaining = CACHE_DURATION - (Date.now() - rcache);
+fs.readFile('./webcam_s.jpg', (err, data) => {
 return res.status(200).send(data);
 });
+} else {
+rcache = Date.now();
+
+
+await NodeWebcam.capture( "webcam_s", opts, function( err, data ) {
+return res.status(200).send(data);
+});
+}
+
 console.log(`${colors.green("[Site]")} function webcam ran`);
 }
