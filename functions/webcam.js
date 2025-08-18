a1901502de1cb6e92cf8c9eeff43c551d1253097
bathtubfulloftoast <bathtubfulloftoast@noreply.localhost>
2025-08-18 21:32:57 +0200

    50 seconds is probably better

--- functions/webcam.js
+++ functions/webcam.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 let rcache;
 
 export default async function lastfmHandler(req, res) {
-const CACHE_DURATION = 30000;
+const CACHE_DURATION = 50000;
 
 var opts = {
     width: 480,
