defad97b04f511b5ae61e7d4240368de3bf3df40
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-10-06 19:40:37 -0600

    trailer area

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,4 +16,19 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const trailers = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/trailers', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		hidden: z.boolean().optional(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, trailers };
