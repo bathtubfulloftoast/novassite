89d9d55dcba37d9293d894635b57a24fe59012c5
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-10-09 12:32:02 -0600

    seasonal splashes!

--- src/pages/splash-list.txt.js
+++ src/pages/splash-list.txt.js
@@ -2,28 +2,54 @@ import { promises as fs } from "fs";
 import path from "path";
 
 export async function GET() {
-    const filePath = path.resolve("src/assets/splashes.txt"); // Adjust based on your project structure
+    const dir = "src/assets/";
+    const splashfile = path.resolve(dir+"splashes.txt"); // Adjust based on your project structure
+    const splashfile_hal = path.resolve(dir+"splashes-hal.txt");
+    const splashfile_chr = path.resolve(dir+"splashes-chrim.txt");
+
     try {
-        const text = await fs.readFile(filePath, "utf-8");
+        const text = await fs.readFile(splashfile, "utf-8");
+        const text_h = await fs.readFile(splashfile_hal, "utf-8");
+        const text_c = await fs.readFile(splashfile_chr, "utf-8");
+
         const splashes = text.split("\n")
         .map(name => name.trim())
         .filter(name => name)
         .map((name, index) => (index+1).toString().padStart(4, " ") + " | " + name);
 
-        // all of this is to make sure people arent downloading thousands of splashes
-        const textContent = splashes.join("\n");
+        const splashes_hal = text_h.split("\n")
+        .map(name => name.trim())
+        .filter(name => name)
+        .map((name, index) => (index+1).toString().padStart(4, " ") + " | " + name);
 
-        const headerstring = `all ${splashes.length} of Novas Splashes!`;
+        const splashes_chr = text_c.split("\n")
+        .map(name => name.trim())
+        .filter(name => name)
+        .map((name, index) => (index+1).toString().padStart(4, " ") + " | " + name);
+
+        // all of this is to make sure people arent downloading thousands of splashes
+        const Content = splashes.join("\n");
+        const Content_h = splashes_hal.join("\n");
+        const Content_c = splashes_chr.join("\n");
+
+        const headerstring = `all ${splashes.length+splashes_hal.length+splashes_chr.length} of Novas Splashes!`;
         const headerlen = headerstring.length;
 
         return new Response(
         `┌${"─".repeat(headerlen+2)}┐\n`+
         `│ ${headerstring} │`+
-        `\n└${"─".repeat(headerlen+2)}┘\n\n`+
+        `\n└${"─".repeat(headerlen+2)}┘`+
 
-        textContent
+        '\n\n== Regular Splashes ==\n'+
+        Content+
+
+        '\n\n== Halloween Splashes ==\n'+
+        Content_h+
+
+        '\n\n== Christmas Splashes ==\n'+
+        Content_c
         );
     } catch (error) {
-        return new Response("Error: File not found");
+        return new Response(error);
     }
 }
--- src/pages/splashes.txt.js
+++ src/pages/splashes.txt.js
@@ -20,21 +20,54 @@ function shuffle(array) {
 
 
 export async function GET() {
-    const filePath = path.resolve("src/assets/splashes.txt"); // Adjust based on your project structure
+const now = new Date();
+const month = now.getMonth()+1;
+// const month = 12;
+
+const dir = "src/assets/";
+    const splash = path.resolve(dir+"splashes.txt"); // Adjust based on your project structure
+    const splash_hal = path.resolve(dir+"splashes-hal.txt"); // sppooooky >:)
+    const splash_chr = path.resolve(dir+"splashes-chrim.txt"); // sppooooky >:)
+
     try {
-        const text = await fs.readFile(filePath, "utf-8");
+        const text = await fs.readFile(splash, "utf-8");
         const splashes = text.split("\n").map(name => name.trim());
 
-let maxsplash = 150;
+        const text_hal = await fs.readFile(splash_hal, "utf-8");
+        const splashes_hal = text_hal.split("\n").map(name => name.trim());
 
-        // all of this is to make sure people arent downloading thousands of splashes
+        const text_chr = await fs.readFile(splash_chr, "utf-8");
+        const splashes_chr = text_chr.split("\n").map(name => name.trim());
+
+let maxsplash = 150;
+if(month == 10 || month == 12) {
+maxsplash = 100;
+}
+
+
+        const hcut = splashes_hal.filter((word) => word.length < 85).filter((word) => word.length > 2);
+        const hshuffle = shuffle(hcut);
+        const hslice = hshuffle.slice(0,maxsplash);
+
+        const ccut = splashes_chr.filter((word) => word.length < 85).filter((word) => word.length > 2);
+        const cshuffle = shuffle(ccut);
+        const cslice = cshuffle.slice(0,maxsplash);
+
         const cut = splashes.filter((word) => word.length < 85).filter((word) => word.length > 2);
         const shuffled = shuffle(cut);
         const sliced = shuffled.slice(0,maxsplash);
-        const textContent = sliced.join("\n");
+
+
+        if (month == 10) {
+        sliced.push.apply(sliced, hslice);
+        } else if (month == 12) {
+        sliced.push.apply(sliced, cslice);
+        }
+
+        const textContent = sliced.join("\n")
 
         return new Response(textContent);
     } catch (error) {
-        return new Response("Error: File not found");
+        return new Response(error);
     }
 }
