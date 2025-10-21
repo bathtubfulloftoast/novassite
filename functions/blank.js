44aea08abbd04fa1588a5fc04aac8322375bef88
bathtubfulloftoast <bathtubfulloftoast@noreply.localhost>
2025-10-21 18:29:33 +0200

    im almost out of money, its been weeks since ive been funny.
    kill yo self ^.^

--- functions/blank.js
+++ functions/blank.js
@@ -1,4 +1,7 @@
 import colors from 'colors';
+// example function that has all the handling and logging
+// no i wont change it from lastfmHandler
+// yes im aware its all the lastfmHandler and changing it is easy as shit
 
 export default async function lastfmHandler(req, res) {
 
