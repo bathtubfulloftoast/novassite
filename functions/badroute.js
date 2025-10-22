eda53fd280dc2ce413126873ea70d2eeb1cf158c
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-10-22 15:37:15 -0600

    yeag

--- functions/badroute.js
+++ functions/badroute.js
@@ -1,13 +1,7 @@
 import colors from 'colors';
 
 export default async function lastfmHandler(req, res) {
-const route = req.params.route;
 
 res.set('Content-Type', "text/plain");
 res.status(404).send("invalid api route\nit was probably removed\nor your request method is incorrect.\n");
-
-if(route) { // only log if it *might* be my fault
-console.log(`${colors.red("[Site]")} function ${route} was called but not found.`);
-}
-
 }
