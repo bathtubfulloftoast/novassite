2bb8d7faca1ee3579f2f44ad506e55ca18f72b47
bathtubfulloftoast <bathtubfulloftoast@noreply.localhost>
2025-10-27 21:01:45 +0100

    fuck you disco rd
    revert longer status so discord might actually fucking show it all the time
    
    its in the api call but not the client half the time
    why??
    who nose

--- discordBot.js
+++ discordBot.js
@@ -77,7 +77,7 @@ client.once('clientReady', () => {
 
     console.log(`${colors.blue("[Discord]")} Logged in as ${client.user.tag}`);
     client.user.setPresence({
-        activities: [{ name: "feeling pretty "+randomface??":3", type: ActivityType.Custom }],
+        activities: [{ name: randomface??":3", type: ActivityType.Custom }],
         status: 'online'
     });
     loggedin = true;
