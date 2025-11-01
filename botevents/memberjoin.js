3c134875a74bbb2769f7cffa319ffc1d60ac4ebe
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-10-31 22:30:10 -0600

    better join and leave messages

--- botevents/memberjoin.js
+++ botevents/memberjoin.js
@@ -1,31 +1,46 @@
-import {EmbedBuilder} from 'discord.js';
+import {EmbedBuilder, AttachmentBuilder} from 'discord.js';
 import colors from 'colors';
 
 const JCHANNEL = process.env.JOINCHANNEL;
 const GUILDID = process.env.PRESENCE_GUILDID;
 
-export function MemberJoin(client) {
-client.on("guildMemberAdd", member => {
+export async function MemberJoin(client) {
+client.on("guildMemberAdd",async member => {
 const jointime = new Date(member.joinedTimestamp);
 
+const hex = Math.floor((Math.abs(Math.sin(member.user.id) * 16777215)));
+
+let image = await fetch(`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.webp?size=256`);
+var pfpbuffer = await image.arrayBuffer();
+pfpbuffer = Buffer.from(pfpbuffer);
+
+let serverimage = await fetch(`https://cdn.discordapp.com/icons/${member.guild.id}/${member.guild.icon}.webp?size=128`);
+var sbuff = await serverimage.arrayBuffer();
+sbuff = Buffer.from(sbuff);
+
+const pfp = await new AttachmentBuilder(pfpbuffer);
+pfp.setName("pfp.webp");
+
+const sicon = await new AttachmentBuilder(sbuff);
+sicon.setName("server.webp");
 
 const MessageEmbed = {
-"description": `Welcome to **${member.guild.name}**!`,
-"color": Math.round(Math.random() * 16777215),
-"title": member.user.globalName,
+"author": {
+"name": `Welcome to ${member.guild.name}!`,
+"icon_url": `attachment://server.webp`
+},
+"title": `Welcome ${member.user.globalName}!`,
 "thumbnail": {
-"url": `https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.webp?size=512`
+"url": `attachment://pfp.webp`
 },
-"footer": {
-"text": member.guild.name,
-"icon_url": `https://cdn.discordapp.com/icons/${member.guild.id}/${member.guild.icon}.webp?size=512`
-},
+"color": hex,
 "timestamp": jointime.toISOString()
 }
 
+
 if(member.guild.id == GUILDID) {
 console.log(`${colors.cyan("[Discord]")} ${member.user.username} joined the server.`);
-client.channels.cache.get(JCHANNEL).send({embeds: [MessageEmbed],content: `Welcome to **${member.guild.name}** <@${member.user.id}>!`});
+client.channels.cache.get(JCHANNEL).send({embeds: [MessageEmbed],files:[pfp,sicon],content: `Welcome to **${member.guild.name}** <@${member.user.id}>!`});
 }
 });
 }
--- botevents/memberleave.js
+++ botevents/memberleave.js
@@ -1,3 +1,4 @@
+import {EmbedBuilder, AttachmentBuilder} from 'discord.js';
 import colors from 'colors';
 
 const JCHANNEL = process.env.JOINCHANNEL;
@@ -6,11 +7,36 @@ const GUILDID = process.env.PRESENCE_GUILDID;
 // i just realized i forgot to do that.
 // okay..?
 
-export function MemberLeave(client) {
-client.on("guildMemberRemove", member => {
+export async function MemberLeave(client) {
+client.on("guildMemberRemove",async member => {
 if(member.guild.id == GUILDID) {
+
+const jointime = new Date(member.joinedTimestamp);
+
+let image = await fetch(`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.webp?size=256`);
+var pfpbuffer = await image.arrayBuffer();
+pfpbuffer = Buffer.from(pfpbuffer);
+
+const pfp = await new AttachmentBuilder(pfpbuffer);
+pfp.setName("pfp.webp");
+
+const MessageEmbed = {
+    "color": 16711680,
+    "author": {
+        "name": `${member.user.globalName} has left the server.`,
+          "icon_url": "attachment://pfp.webp"
+    },
+    "footer": {
+        "text": `join date`
+    },
+    "timestamp": jointime.toISOString()
+};
+
 console.log(`${colors.cyan("[Discord]")} ${member.user.username} left the server.`);
-client.channels.cache.get(JCHANNEL).send(`<@${member.user.id}> (aka **${member.user.globalName}**) left the server.`);
+client.channels.cache.get(JCHANNEL).send({files: [pfp], embeds: [MessageEmbed]});
+
+
+
 }
 });
 }
