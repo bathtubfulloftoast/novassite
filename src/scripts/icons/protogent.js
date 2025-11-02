9b1a9549f8b9dbeff34cbaf4cf3fb854145f9294
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-11-01 23:16:10 -0600

    install my antivirus

---
+++ src/scripts/icons/protogent.js
@@ -0,0 +1,25 @@
+async function createaudio() {
+
+const audioElement = await new Audio("/media/sfx/blips.ogg");
+audioElement.volume = 0.3;
+
+const response = await fetch('/404');
+const data = await response.text();
+
+audioElement.addEventListener('ended', (event) => {
+window.open("https://www.youtube.com/watch?v=jyZun_uFzac&t=7s", '_blank');
+location.reload();
+});
+
+await document.write(data);
+await audioElement.play();
+
+
+
+};
+
+document.getElementById("protogent").addEventListener("click", function(event) {
+event.preventDefault();
+createaudio();
+});
+
