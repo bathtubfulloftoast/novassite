aaafe3ab73631e75a10d3b6553e272bc709dbb80
bathtubfulloftoast <bathtubfulloftoast@noreply.localhost>
2025-11-18 16:09:36 +0100

    closer to an actual month

--- src/scripts/stats.js
+++ src/scripts/stats.js
@@ -15,7 +15,7 @@ const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const week = day * 7;
-const month = week * 4;
+const month = week * 4.4;
 const year = month * 12;
 
 if (uptime >= year) {
