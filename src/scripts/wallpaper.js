ca551cbcac2c339ccfea61595987e2a178a102e0
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-11-24 17:50:27 -0700

    no matter what we do we all end up on someone elses plate ^.^

--- src/scripts/wallpaper.js
+++ src/scripts/wallpaper.js
@@ -1,9 +1,31 @@
 document.addEventListener("DOMContentLoaded", async function() {
 const now = new Date();
 const month = now.getMonth()+1;
-// const month = 6;
+const day = now.getDate();
+const date = `${month}-${day}`;
 
 const paperdoll = document.getElementById("bnuy");
+
+if(date == "12-25") {
+document.body.style.background = "#fff";
+paperdoll.style.backgroundImage = "url('/media/wallpapers/mariah.png')";
+paperdoll.style.backgroundSize = "45vh";
+paperdoll.style.backgroundPosition = "center";
+
+document.getElementById("novasprofile").src="https://www.youtube.com/embed/aAkMkVFwAoo?si=kGSB-eRC5Eat_jPB";
+document.getElementById("novasprofile").height="450px";
+
+document.querySelectorAll(".icon").forEach((icon) => {
+icon.querySelector("img").src = "/media/icons/christmas.png";
+icon.querySelector("img").alt = "mariah carey";
+
+icon.querySelector("span").innerHTML = "mariah.carey";
+
+
+});
+
+return;
+}
 
 
 if([3,4,5].includes(month)) {//spring
