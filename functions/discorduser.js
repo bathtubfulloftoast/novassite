04dd1e40a772d9a629c41eece82a7e28422dc187
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-11-24 18:01:15 -0700

    nevermidn i forgot how good it was to know things

--- functions/discorduser.js
+++ functions/discorduser.js
@@ -27,7 +27,7 @@ export default async function lastfmHandler(req, res) {
         const remaining = CACHE_DURATION - (Date.now() - cache[userid].timestamp);
         return res.status(200).json({
             ...cache[userid].data,
-            // cache_remaining: Math.floor(remaining/1000),
+            cache_remaining: Math.floor(remaining/1000),
         });
     }
 
@@ -51,7 +51,7 @@ export default async function lastfmHandler(req, res) {
 
         res.status(200).json({
             ...data,
-            // cache_remaining: Math.floor(CACHE_DURATION/1000),
+            cache_remaining: Math.floor(CACHE_DURATION/1000),
         });
         console.log(`${colors.green("[Site]")} grabbed info for discord userid ${userid}`);
 
