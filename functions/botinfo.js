c3cc047af56cd9ccaa399050e8cb1fc4c21b9912
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-12-03 13:46:34 -0700

    better caching.
    also the /404 issue isnt even my fault so thats fucking sweet

--- functions/botinfo.js
+++ functions/botinfo.js
@@ -5,16 +5,13 @@ import { client } from '../discordBot.js';
 let cache = {};
 
 export default async function presenceHandler(req, res) {
-    const CACHE_DURATION = 60000;
 
-    res.set('Cache-Control', "max-age="+(CACHE_DURATION/1000));
+    res.set('Cache-Control', "max-age="+(60));
 
     // Check for valid cache
-    if (cache.timestamp && (Date.now() - cache.timestamp < CACHE_DURATION)) {
-        const remaining = CACHE_DURATION - (Date.now() - cache.timestamp);
+    if (cache.cached) {
         return res.status(200).json({
             ...cache.data,
-            cache_remaining: Math.floor(remaining / 1000),
         });
     }
 
@@ -28,20 +25,28 @@ export default async function presenceHandler(req, res) {
             activities: client.presence.activities,
         };
 
+        if (data.user.id) {
         cache = {
-            data,
-            timestamp: Date.now(),
+        data,
+        cached:true,
+        timestamp: Date.now(),
         };
+        } else {
+        return res.status(500).json({
+        error:"couldnt grab all the bots info (its probably not online yet.)"
+        });
+        }
+
 
         res.status(200).json({
-            ...data,
-            cache_remaining: Math.floor(CACHE_DURATION / 1000),
+        ...data,
         });
+
         console.log(`${colors.green("[Site]")} grabbed bot info`);
 
     } catch (error) {
         console.error('Error fetching presence:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'couldnt grab the bots info (its probably not online yet.)' });
         console.log(`${colors.red("[ERROR]")} unable to grab bot info (are you logged in yet?)`);
 
     }
