e3e6c2414c605cba3b59b257430b7795064a03e5
bathtubfulloftoast <bathtubfulloftoast@noreply.localhost>
2025-12-08 19:01:31 +0100

    daytime cycle to novassite

--- src/scripts/wallpaper.js
+++ src/scripts/wallpaper.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", async function() {
 const now = new Date();
 const month = now.getMonth()+1;
 const day = now.getDate();
+const hour = now.getHours();
+var minute= now.getMinutes();
 const date = `${month}-${day}`;
+minute = minute/60;
+
+const percent = (hour+minute)/24;
+const deg = Math.round(percent*360);
+// lets hope this math works out :D
 
 const paperdoll = document.getElementById("bnuy");
 
@@ -42,13 +49,13 @@ return;
 
 
 if([3,4,5].includes(month)) {//spring
-document.body.style.backgroundImage = "linear-gradient(-45deg,var(--bg),var(--bright-blue))";
+document.body.style.backgroundImage = `linear-gradient(${deg}deg,var(--bg),var(--bright-blue))`;
 
 paperdoll.style.backgroundImage = "url('/media/wallpapers/bunny.png')";
 }
 
 else if([6,7,8].includes(month)) {//summer
-document.body.style.backgroundImage = "linear-gradient(190deg,var(--fg0),var(--aqua))";
+document.body.style.backgroundImage = `linear-gradient(${deg}deg,var(--fg0),var(--aqua))`;
 
 paperdoll.style.backgroundImage = "url('/media/wallpapers/bird.png')";
 paperdoll.style.backgroundSize = "70vh";
@@ -56,13 +63,13 @@ paperdoll.style.backgroundSize = "70vh";
 }
 
 else if([9,10,11].includes(month)) {//fall
-document.body.style.backgroundImage = "linear-gradient(128deg,var(--bg),var(--bright-orange))";
+document.body.style.backgroundImage = `linear-gradient(${deg}deg,var(--bg),var(--bright-orange))`;
 
 paperdoll.style.backgroundImage = "url('/media/wallpapers/demon.png')";
 }
 
 else if([12,1,2].includes(month)) {//winter
-document.body.style.backgroundImage = "linear-gradient(128deg,var(--bg),var(--purple))";
+document.body.style.backgroundImage = `linear-gradient(${deg}deg,var(--bg),var(--purple))`;
 
 paperdoll.style.backgroundImage = "url('/media/wallpapers/deer.png')";
 paperdoll.style.backgroundPosition = "right bottom";
