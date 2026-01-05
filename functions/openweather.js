455ddcc1915cec4bbb1931214c18bb94fa9b68a1
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2026-01-04 20:40:44 -0700

    me when i make arbitrary changes for no reason

--- functions/openweather.js
+++ functions/openweather.js
@@ -5,7 +5,7 @@ const key = process.env.OPENWEATHER_API_KEY;
 const cityid = "5476913";
 const cache = 3600000; // 1 hour
 
-const url = `http://api.openweathermap.org/data/2.5/weather?id=${CITYID}&units=imperial&appid=${API_KEY}`;
+const url = `http://api.openweathermap.org/data/2.5/weather?id=${cityid}&units=imperial&appid=${key}`;
 
 export default async function swag(req, res) {
 const fetched = await fetcher(cache,url,"openweathermap");
